94649bcb701d47a48a7c3466a5f92cec119d1fea
Ahmed Roshdy <ahmedroshdy654@gmail.com>
2025-03-25 01:50:36 +0200

    updating styles

--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_doctor/core/theming/colors.dart';
+import 'package:my_doctor/core/theming/font_weight_helper.dart';
 
 class TextStyles {
   static TextStyle font24Black700weight = TextStyle(
     fontSize: 24.sp,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeightHelper.bold,
     color: Colors.black,
   );
   static TextStyle font32BlueBold = TextStyle(
-    fontSize: 32.sp,
-    fontWeight: FontWeight.bold,
+    fontSize: 24.sp,
+    fontWeight: FontWeightHelper.bold,
+    color: ColorsManager.mainBlue,
+  );
+  static TextStyle font24BlueBold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeightHelper.bold,
     color: ColorsManager.mainBlue,
   );
   static TextStyle font12GrayRegular = TextStyle(
     fontSize: 12.sp,
-    fontWeight: FontWeight.normal,
+    fontWeight: FontWeightHelper.normal,
     color: ColorsManager.gray,
   );
   static TextStyle font16WhiteSemiBold = TextStyle(
     fontSize: 16.sp,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeightHelper.medium,
     color: Colors.white,
   );
 }
